3ce156dad3a145735fe7c42d59a3af2d79e96f18
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-04-13 12:08:59 +0530

    Projection Operator

--- bin/main.dart
+++ bin/main.dart
@@ -6,7 +6,12 @@ import 'package:function_collections/people.dart';
 
 void main(List<String> arguments) {
 
+  //where operator
   var result=peopleList.where((onePeople)=>(onePeople.age>20) && (onePeople.id>1));
   print(result);
+
+  //projection operator
+  var result1=peopleList.map((onePeople)=>onePeople.name);
+  print(result1);
   
 }
