c2b09b50f5eb6d3d85f7f070fa70abbb8ad24e3d
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-04-13 13:15:11 +0530

    Intersection

--- bin/main.dart
+++ bin/main.dart
@@ -4,7 +4,7 @@ void main(List<String> arguments) {
   var num=[1,1,1,4,5,5];
   var num1=[1,2,3];
   var res=num.toSet();
-  var res1=num.toSet().union(num1.toSet());
+  var res1=num.toSet().intersection(num1.toSet());
   print(res1);
 
   
