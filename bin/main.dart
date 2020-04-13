7f101ac412af36ac51bab5c538498bd680e4b84e
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-04-13 12:05:03 +0530

    .where with two condition

--- bin/main.dart
+++ bin/main.dart
@@ -6,7 +6,7 @@ import 'package:function_collections/people.dart';
 
 void main(List<String> arguments) {
 
-  var result=peopleList.where((onePeople)=>(onePeople.age>20));
+  var result=peopleList.where((onePeople)=>(onePeople.age>20) && (onePeople.id>1));
   print(result);
   
 }
