ce9ce2dfbbe8b0a0dd8c736fd1f8173d18ae7013
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-04-13 11:43:00 +0530

    where

--- bin/main.dart
+++ bin/main.dart
@@ -3,10 +3,8 @@ void main(List<String> arguments) {
 
   var numbers=[4,3,1,7,8,2,0,9,5,6];
 
-
-  for(var no in numbers){
-    if(no>5){
-      print(no);
-    }
-  }
+  var result=numbers.where((no)=>no>5);
+  print(result);
+  result.forEach(print);
+  
 }
